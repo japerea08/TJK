69268bbddadae740948e3d29b0895948ddbac104
Joey Perea <j.perea08@gmail.com>
2018-07-07 10:14:01 -0400

    modified the user create account html

---
+++ db/user_seeds.sql
@@ -0,0 +1,7 @@
+use tjk_db;
+
+INSERT into users VALUES("dhxaFpN2CrSYyrjTETBgJBtcSyH3", "Thomas J Kelly", "Joey", "Perea", "9445 sw 100 st", "Miami", "Fl", "33176", "tjkperea@gmail.com", "7864931114", 1);
+
+use tjk_db;
+
+INSERT into users VALUES("dhxaFpN2CrSYyrjTETBgJBtcSyH3", "Thomas J Kelly", "Joey", "Perea", "9445 sw 100 st", "Miami", "Fl", "33176", "tjkperea@gmail.com", "7864931114", 1);
